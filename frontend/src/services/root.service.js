22f6964800df7faaf6ae5a9d5c3127bcb157c0d9
LuisFernandez <luis.fernandez2101@alumnos.ubiobio.cl>
2025-07-31 21:22:17 -0400

    pruebas 29

--- frontend/src/services/root.service.js
+++ frontend/src/services/root.service.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import cookies from 'js-cookie';
 
-const API_URL = import.meta.env.VITE_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const instance = axios.create({
-  baseURL: API_URL,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
